60266b886d7aeddd353b40c8ef6a245a82fde915
Sebastian Vivas <sebastian@avilatek.dev>
2022-04-12 17:26:03 -0400

    delete unused import

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -15,9 +15,6 @@ use near_contract_standards::non_fungible_token::{Token, TokenId};
 // HashMap import
 use std::collections::HashMap;
 
-pub use crate::nfts::*;
-mod nfts;
-
 near_sdk::setup_alloc!();
 
 // Objects
