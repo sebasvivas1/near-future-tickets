61dd6033a0780cd07c76244f28510e763d6f6aa1
Sebastian Vivas <sebastian@avilatek.dev>
2022-04-16 19:16:12 -0400

    improve layout to allow collections

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,3 +1,4 @@
+use near_sdk::collections::UnorderedSet;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::Serialize;
 use near_sdk::serde::Deserialize;
@@ -15,6 +16,7 @@ use near_contract_standards::non_fungible_token::{Token, TokenId};
 // HashMap import
 use std::collections::HashMap;
 
+pub type TokenSeriesId = String;
 
 // Objects
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
@@ -43,6 +45,49 @@ pub struct Event {
     organizer: AccountId
 }
 
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct TokenSeries {
+	metadata: TokenMetadata,
+	creator_id: AccountId,
+	tokens: UnorderedSet<TokenId>,
+    price: Option<Balance>,
+    is_mintable: bool,
+    royalty: HashMap<AccountId, u32>
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct TokenSeriesJson {
+    token_series_id: TokenSeriesId,
+	metadata: TokenMetadata,
+	creator_id: AccountId,
+    royalty: HashMap<AccountId, u32>
+}
+
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Market {
+    token_series_id: TokenSeriesId,
+    metadata: TokenMetadata,
+    owner_id: AccountId,
+    creator_id: AccountId,
+    price: Balance,
+    royalty: HashMap<AccountId, u32>,
+    copy: i64,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct MarketJson {
+    token_series_id: TokenSeriesId,
+    metadata: TokenMetadata,
+    owner_id: AccountId,
+    creator_id: AccountId,
+    price: Balance,
+    royalty: HashMap<AccountId, u32>,
+}
+
+
 // Creo que falta algo de ticket ???
 
 // -------- Objects End --------- //
@@ -63,6 +108,7 @@ pub struct Contract {
     // NFT (Ticket)
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    token_series_by_id: UnorderedMap<TokenSeriesId, TokenSeries>
 }
 
 impl Default for NEARFT {
@@ -81,6 +127,9 @@ enum StorageKey {
     TokenMetadata,
     Enumeration,
     Approval,
+    TokenSeriesById,
+    TokenBySeriesInner { token_series: String },
+    TokensPerOwner { account_hash: Vec<u8>},
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -94,7 +143,7 @@ impl Contract {
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
                 name: "near-future-tickets".to_string(),
-                symbol: "NFT".to_string(),
+                symbol: "NEAR Future Tickets".to_string(),
                 icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
                 base_uri: None,
                 reference: None,
@@ -116,6 +165,7 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            token_series_by_id: UnorderedMap::new(StorageKey::TokenSeriesById),
         }
     }
 
