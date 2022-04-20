4f5e9173e8b7eeca3b0ceb398b3a47383aa39f54
JuEnPeHa <jeph@nearlearning.app>
2022-04-19 20:34:32 -0500

    Please verify now _ 5

--- frontend/components/HomePage/HomePage.tsx
+++ frontend/components/HomePage/HomePage.tsx
@@ -2,8 +2,6 @@ import Layout from "../Landing/Layout";
 
 export default function HomePage() {
     return (
-        <div className="min-h-screen min-w-screen bg-figma-200">
-            
-        </div>
+        <div className=""></div>
     )
 }
--- frontend/pages/home_page.tsx
+++ frontend/pages/home_page.tsx
@@ -8,6 +8,5 @@ export default function home() {
     <HomePage></HomePage>
     </Layout>   
     </div>
-      
   );
 }
