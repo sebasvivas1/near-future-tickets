c39b397454ea42eca67a70608293f1baa9356919
Sebastian Vivas <sebastian@avilatek.dev>
2022-04-22 21:43:15 -0400

    Mint Tickets and add nft to wallet

--- nft-contract/src/lib.rs
+++ nft-contract/src/lib.rs
@@ -1,4 +1,5 @@
 use near_contract_standards::non_fungible_token::{ NonFungibleToken };
+use near_contract_standards::non_fungible_token::metadata::TokenMetadata as tokenmeta;
 use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
@@ -60,7 +61,7 @@ pub struct TokenSeries {
 	creator_id: AccountId,
     tokens: UnorderedSet<TokenId>,
     is_mintable: bool,
-    // royalty: HashMap<AccountId, u32>,
+    royalty: HashMap<AccountId, u32>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
@@ -102,7 +103,8 @@ pub struct Contract {
 /// Helper structure for keys of the persistent collections.
 #[derive(BorshSerialize, BorshStorageKey)]
 pub enum StorageKey {
-    TokensPerOwner,
+    TokensPerOwner { account_hash: Vec<u8> },
+    TokensPerOwnerr,
     TokenPerOwnerInner { account_id_hash: CryptoHash },
     TokensById,
     TokenMetadataById,
@@ -143,7 +145,7 @@ impl Contract {
         let this = Self {
             tokens: NonFungibleToken::new(StorageKey::NonFungibleToken, owner_id.clone(), Some(StorageKey::TokenMetadata), Some(StorageKey::Enumeration), Some(StorageKey::Approval),),
             events: UnorderedMap::new(StorageKey::Events),
-            tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwner),
+            tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwnerr),
             tokens_by_id: LookupMap::new(StorageKey::TokensById),
             token_metadata_by_id: UnorderedMap::new(
                 StorageKey::TokenMetadataById.try_to_vec().unwrap(),
@@ -172,7 +174,8 @@ this
         banner: String,
         ticket_type: Vec<String>,
         ticket_banners: Vec<String>,
-        // royalty: Option<HashMap<AccountId, u32>>,
+        //we add an optional parameter for perpetual royalties
+        perpetual_royalties: Option<HashMap<AccountId, u32>>,
      ) -> Event {
         // Initial storage usage
         let initial_storage_usage = env::storage_usage();
@@ -180,6 +183,20 @@ this
         let index = i128::from(self.events.len() + 1);
         let mut children_token_map = Vec::new();
         let mut metadata = token_metadata;
+
+        // create a royalty map to store in the token
+        let mut royalty = HashMap::new();
+
+        // if perpetual royalties were passed into the function:
+        if let Some(perpetual_royalties) = perpetual_royalties {
+            //make sure that the length of the perpetual royalties is below 7 since we won't have enough GAS to pay out that many people
+            assert!(perpetual_royalties.len() < 7, "Cannot add more than 6 perpetual royalty amounts");
+
+            //iterate through the perpetual royalties and insert the account and amount in the royalty map
+            for (account, amount) in perpetual_royalties {
+                royalty.insert(account, amount);
+            }
+        }
 
         for (i, _x) in ticket_type.iter().enumerate() {
 
@@ -202,6 +219,7 @@ this
                     .unwrap(),
                 ),
                 is_mintable: true,
+                royalty: royalty.clone(),
             });
 
             children_token_map.push(TokenSeriesJson{
@@ -211,7 +229,7 @@ this
                 token_series_id,
             });
         }
-        // refund_deposit(initial_storage_usage);
+
 
         let event = Event {
             name: name,
@@ -228,7 +246,139 @@ this
             tickets: children_token_map,
         };
         self.events.insert(&event.index, &event);
+        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
+        refund_deposit(required_storage_in_bytes);
+
         event
+    }
+
+    #[payable]
+    pub fn mint_ticket(
+        &mut self,
+        token_series_id: TokenSeriesId,
+        receiver_id: AccountId
+    ) -> TokenId {
+        let token_series = self.token_series_by_id.get(&token_series_id).expect("Token series not exist");
+        assert_eq!(env::predecessor_account_id(), token_series.creator_id, "not creator");
+        let token_id: TokenId = self._nft_mint_series(token_series_id, receiver_id);
+        token_id
+    }
+
+    fn _nft_mint_series(
+        &mut self,
+        token_series_id: TokenSeriesId,
+        receiver_id: AccountId
+    ) -> TokenId {
+        let mut token_series = self.token_series_by_id.get(&token_series_id).expect("Token series not exist");
+        assert!(
+            token_series.is_mintable,
+            "Token series is not mintable"
+        );
+
+        let num_tokens = token_series.tokens.len();
+        let max_copies = token_series.metadata.copies.unwrap_or(u64::MAX);
+        assert!(num_tokens < max_copies, "Series supply maxed");
+
+        if (num_tokens + 1) >= max_copies {
+            token_series.is_mintable = false;
+        }
+
+        let token_id = format!("{}{}{}", &token_series_id, TITLE_DELIMETER, num_tokens + 1);
+        token_series.tokens.insert(&token_id);
+        self.token_series_by_id.insert(&token_series_id, &token_series);
+        let title: String = format!("{:?} - {}{}{}{}", token_series.metadata.title.clone(), TITLE_DELIMETER, &token_series_id, TITLE_DELIMETER, (num_tokens + 1).to_string());
+
+
+        let metadata = tokenmeta {
+            title: Some(title.clone()),
+            description: token_series.metadata.description.clone(),
+            media: token_series.metadata.media.clone(),
+            media_hash: token_series.metadata.media_hash.clone(),
+            copies: token_series.metadata.copies.clone(),
+            issued_at: Some(env::block_timestamp().to_string()),
+            expires_at:None,
+            starts_at: None,
+            updated_at: None,
+            extra: token_series.metadata.extra.clone(),
+            reference: token_series.metadata.reference.clone(),
+            reference_hash: token_series.metadata.reference_hash.clone(),
+        };
+        let metadataa = TokenMetadata {
+            title: Some(title),
+            description: token_series.metadata.description.clone(),
+            media: token_series.metadata.media.clone(),
+            media_hash: token_series.metadata.media_hash.clone(),
+            copies: token_series.metadata.copies.clone(),
+            issued_at: Some(env::block_timestamp()),
+            expires_at:None,
+            starts_at: None,
+            updated_at: None,
+            extra: token_series.metadata.extra.clone(),
+            reference: token_series.metadata.reference.clone(),
+            reference_hash: token_series.metadata.reference_hash.clone(),
+        };
+
+        let owner_id: AccountId = receiver_id;
+        self.tokens.owner_by_id.insert(&token_id, &owner_id);
+
+        self.tokens
+            .token_metadata_by_id
+            .as_mut()
+            .and_then(|by_id| by_id.insert(&token_id, &metadata));
+
+         if let Some(tokens_per_owner) = &mut self.tokens.tokens_per_owner {
+             let mut token_ids = tokens_per_owner.get(&owner_id).unwrap_or_else(|| {
+                 UnorderedSet::new(StorageKey::TokensPerOwner {
+                     account_hash: env::sha256(&owner_id.as_bytes()),
+                 })
+             });
+             token_ids.insert(&token_id);
+             tokens_per_owner.insert(&owner_id, &token_ids);
+         };
+
+         let token = Token {
+            //set the owner ID equal to the receiver ID passed into the function
+            owner_id: owner_id.clone(),
+            //we set the approved account IDs to the default value (an empty map)
+            approved_account_ids: Default::default(),
+            //the next approval ID is set to 0
+            next_approval_id: 0,
+            //the map of perpetual royalties for the token (The owner will get 100% - total perpetual royalties)
+            royalty: token_series.royalty,
+        };
+
+        //insert the token ID and token struct and make sure that the token doesn't exist
+        assert!(
+            self.tokens_by_id.insert(&token_id, &token).is_none(),
+            "Token already exists"
+        );
+         //insert the token ID and metadata
+         self.token_metadata_by_id.insert(&token_id, &metadataa);
+
+         //call the internal method for adding the token to the owner
+         self.internal_add_token_to_owner(&token.owner_id, &token_id);
+
+         // Construct the mint log as per the events standard.
+         let nft_mint_log: EventLog = EventLog {
+             // Standard name ("nep171").
+             standard: NFT_STANDARD_NAME.to_string(),
+             // Version of the standard ("nft-1.0.0").
+             version: NFT_METADATA_SPEC.to_string(),
+             // The data related with the event stored in a vector.
+             event: EventLogVariant::NftMint(vec![NftMintLog {
+                 // Owner of the token.
+                 owner_id: token.owner_id.to_string(),
+                 // Vector of token IDs that were minted.
+                 token_ids: vec![token_id.to_string()],
+                 // An optional memo to include.
+                 memo: None,
+             }]),
+         };
+
+         // Log the serialized json.
+         env::log_str(&nft_mint_log.to_string());
+
+        token_id
     }
 
     // Get All events
