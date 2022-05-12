68c3c48684274f0fbfb33a166057da3a70b0c0f5
Samuel Navas <samuelnavas1@gmail.com>
2022-05-11 21:56:41 -0400

    no funciona

--- frontend/components/Common/Footer.tsx
+++ frontend/components/Common/Footer.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 export default function () {
    return (
     <div className="">
-      <div className="absolute left-0 top-20 lg:bottom-36">
+      <div className=" left-0 top-20 lg:bottom-36">
         <img src="/carousel.png" alt="" className="w-64 lg:w-auto" />
         </div>    
-          <h2 className=" absolute inset-x-40 bottom-20 h-96 text-figma-300 font-semibold lg:text-7xl">
+          <h2 className="  inset-x-40 bottom-20 h-96 text-figma-300 font-semibold lg:text-7xl">
           Events title
           </h2>
-            <h2 className=" absolute  inset-x-40 bottom-28 h-56 top-50  text-figma-300 ">
+            <h2 className="   inset-x-40 bottom-28 h-56 top-50  text-figma-300 ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae rerum nemo qui impedit sunt ipsa corrupti dolorem, maxime tempora magni, debitis culpa laboriosam similique atque asperiores cum officiis molestias voluptates!
             </h2>
-         <div className="absolute top-40 right-40  bottom-80 ">
+         <div className=" top-40 right-40  bottom-80 ">
             <img src="/event.png" alt="" className=" w-64 lg:w-auto " />    
          </div>
         <div className="text-right  top-80 right-40  bottom-40 mt-8 lg:mt-14 relative ">
@@ -30,7 +30,19 @@ export default function () {
             Buy Now
           </button>
         </div>
-        <div className=" absolute  inset-x-40 bottom-0 ">  
+        <div className="  inset-x-40 bottom-0 ">  
+            <h2 className=' text-figma-400 text-3xl  '>  
+              Upcoming Events
+            </h2>
+            <h2 className=' text-figma-400 text-3xl  '>  
+              Upcoming Events
+            </h2>
+            <h2 className=' text-figma-400 text-3xl  '>  
+              Upcoming Events
+            </h2>
+            <h2 className=' text-figma-400 text-3xl  '>  
+              Upcoming Events
+            </h2>
             <h2 className=' text-figma-400 text-3xl  '>  
               Upcoming Events
             </h2>
